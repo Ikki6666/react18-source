23981923aae2c643153699480cc9c126141b55b0
Your Name <you@example.com>
2022-10-21 22:38:18 +0800

    init

--- 1.react/src/main.jsx
+++ 1.react/src/main.jsx
@@ -1,23 +1,17 @@
+import * as React from 'react';
 import { createRoot } from "react-dom/client";
+function counter(state, action) {
+  if (action.type === 'add') return state + action.payload;
+  return state;
+}
 function FunctionComponent() {
-  // hooks 用到更新 更新需要有事件触发
-  return (
-    <h1
-      onClick={(event) => console.log(`ParentBubble`)}
-      onClickCapture={(event) => {
-        console.log(`ParentCapture`)
-        event.stopPropagation();
-      }}
-    >
-      <span
-        onClick={(event) => {
-          console.log(`ChildBubble`);
-          event.stopPropagation();
-        }}
-        onClickCapture={(event) => console.log(`ChildCapture`)}
-      >world</span>
-    </h1>
-  )
+  const [number, setNumber] = React.useReducer(counter, 0);
+  const [number2, setNumber2] = React.useReducer(counter, 0);
+  return <button onClick={() => {
+    setNumber({ type: 'add', payload: 1 });//update1=>update2=>update3=>update1
+    setNumber({ type: 'add', payload: 1 });//update2
+    setNumber({ type: 'add', payload: 1 });//update3 
+  }}>{number}</button>
 }
 let element = <FunctionComponent />
 //old let element = React.createElement(FunctionComponent);
--- 1.react/src/react-reconciler/src/ReactFiberHooks.js
+++ 1.react/src/react-reconciler/src/ReactFiberHooks.js
@@ -1,3 +1,50 @@
+import ReactSharedInternals from "shared/ReactSharedInternals";
+
+const { ReactCurrentDispatcher } = ReactSharedInternals;
+let currentRenderingFiber = null;
+let workInProgressHook = null;
+
+const HooksDispatcherOnMount = {
+  useReducer: mountReducer
+}
+function mountReducer(reducer, initialArg) {
+  const hook = mountWorkInProgressHook();
+  hook.memoizedState = initialArg;
+  const queue = {
+    pending: null
+  }
+  hook.queue = queue;
+  const dispatch = dispatchReducerAction.bind(null, currentRenderingFiber, queue);
+  return [hook.memoizedState, dispatch];
+}
+/**
+ * 执行派发动作的方法，它要更新状态，并且让界面重新更新
+ * @param {*} fiber function对应的fiber
+ * @param {*} queue hook对应的更新队列
+ * @param {*} action 派发的动作
+ */
+function dispatchReducerAction(fiber, queue, action) {
+  console.log(fiber, queue, action);
+}
+/**
+ * 挂载构建中的hook
+ * */
+function mountWorkInProgressHook() {
+  const hook = {
+    memoizedState: null,//hook的状态 0
+    queue: null,//存放本hook的更新队列 queue.pending=update的循环链表
+    next: null //指向下一个hook,一个函数里可以会有多个hook,它们会组成一个单向链表
+  };
+  if (workInProgressHook === null) {
+    //当前函数对应的fiber的状态等于第一个hook对象
+    currentRenderingFiber.memoizedState = workInProgressHook = hook;
+  } else {
+    workInProgressHook.next = hook;
+    workInProgressHook = hook;
+    workInProgressHook = workInProgressHook.next = hook;
+  }
+  return workInProgressHook;
+}
 /**
  * 渲染函数组件
  * @param {*} current 老fiber
@@ -7,6 +54,9 @@
  * @returns 虚拟DOM或者说React元素
  */
 export function renderWithHooks(current, workInProgress, Component, props) {
+  currentRenderingFiber = workInProgress;//Function组件对应的fiber
+  ReactCurrentDispatcher.current = HooksDispatcherOnMount;
+  //需要要函数组件执行前给ReactCurrentDispatcher.current赋值
   const children = Component(props);
   return children;
 }
